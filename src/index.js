9e655ee65e2f3114ab51747d9b30e93c41d087fa
Peter Vang <vang_peter06@yahoo.com>
2022-07-01 13:21:57 -0700

    WIP working on displaying the correct exchange output

--- src/index.js
+++ src/index.js
@@ -6,9 +6,15 @@ import CurrencyService from './currencyService';
 
 
 
-function getElements(response) {
-  const USDollar = $('#USNumber').val();
-  if (response.conversion_rates) {
+function getElements(response, USDollar, InputCurrency) {
+  // const USDollar = $('#USNumber').val();
+  // const InputCurrency = $('#currency').val();
+  if (response["conversion_rates"][InputCurrency] === undefined) {
+    $('.showErrors').text(`The Currency You selected is not available`);
+  } else if (response.result === "success") {
+  
+    // $('.showExchange').text(`${(response.conversion_rates.EUR * USDollar).toFixed(2)} Euro`);
+
     $('.showExchange').text(`The exchange for Euro is ${(response.conversion_rates.EUR * USDollar).toFixed(2)} Euro`);
     $('.showExchange').text(`The exchange for Japanese Yen is ${(response.conversion_rates.JPY * USDollar).toFixed(2)} Yen`);
     $('.showExchange').text(`The exchange for Mexico Peso is ${(response.conversion_rates.MXN * USDollar).toFixed(2)} Peso`);
@@ -19,14 +25,21 @@ function getElements(response) {
   }
 }
 
+function clearFields() {
+  $('.showExchange').text("");
+  $('.showError').text("");
+}
 async function makeApiCall() {
   const response = await CurrencyService.currencyExchange();
-  getElements(response);
+  getElements(response, USDollar, InputCurrency);
 }
 
 $(document).ready(function() {
   $('#exchange').click(function() {
-    makeApiCall();
+    const USDollar = $('#USNumber').val();
+    const InputCurrency = $('#currency').val();
+    clearFields;
+    makeApiCall(response, USDollar, InputCurrency);
   });
 });
 
