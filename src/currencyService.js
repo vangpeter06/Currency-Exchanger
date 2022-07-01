258795fa54c493d683530beec0ae1ab8d1944505
Peter Vang <vang_peter06@yahoo.com>
2022-07-01 11:18:28 -0700

    change Api number to Process.evn.API_KEY

--- src/currencyService.js
+++ src/currencyService.js
@@ -1,14 +1,14 @@
 export default class CurrencyService {
   static currencyExchange() {
-    return fetch('https://v6.exchangerate-api.com/v6/6497ae9f0e059b9771e1c0f1/latest/USD')
-    .then(Response => {
-      if (!response.ok){
-        throw Error("ERROR");
-      }
-      return response.json();
-    })
-    .catch( error => {
-      return error.message;
-    })
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      .then(response => {
+        if (!response.ok){
+          throw Error("ERROR");
+        }
+        return response.json();
+      })
+      .catch( error => {
+        return error.message;
+      });
   }
 }    
