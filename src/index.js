abcf7bf2513ae4c6b889251cc5fced304fc00c9e
Peter Vang <vang_peter06@yahoo.com>
2022-07-01 14:29:12 -0700

    WIP working on displaying the correct exchange output

--- src/index.js
+++ src/index.js
@@ -6,20 +6,29 @@ import CurrencyService from './currencyService';
 
 
 
-function getElements(response, USDollar, InputCurrency) {
-  // const USDollar = $('#USNumber').val();
-  // const InputCurrency = $('#currency').val();
-  if (response["conversion_rates"][InputCurrency] === undefined) {
+function getElements(response) {
+  let dollar = $('#USNumber').val();
+  let inputCurrency = $('#currency').val();
+  let currencyArray = [];
+  console.log(inputCurrency);
+  console.log(response);
+  for (let i = 0; i < response.conversion_rates.length; i++) {
+    currencyArray.push(`${response.conversion_rates}`);
+    console.log(currencyArray);
+  }
+  if (response["conversion_rates"][inputCurrency] === undefined) {
     $('.showErrors').text(`The Currency You selected is not available`);
-  } else if (response.result === "success") {
-  
+  } else if (response.conversion_rates === "inputCurrency") {
+    console.log(response.conversion_rates())
+    for (inputCurrency of inputCurrency) {
     // $('.showExchange').text(`${(response.conversion_rates.EUR * USDollar).toFixed(2)} Euro`);
 
-    $('.showExchange').text(`The exchange for Euro is ${(response.conversion_rates.EUR * USDollar).toFixed(2)} Euro`);
-    $('.showExchange').text(`The exchange for Japanese Yen is ${(response.conversion_rates.JPY * USDollar).toFixed(2)} Yen`);
-    $('.showExchange').text(`The exchange for Mexico Peso is ${(response.conversion_rates.MXN * USDollar).toFixed(2)} Peso`);
-    $('.showExchange').text(`The exchange for Thai Baht is ${(response.conversion_rates.THB * USDollar).toFixed(2)} Baht`);
-    $('.showExchange').text(`The exchange for China Yuan is ${(response.conversion_rates.CNY * USDollar).toFixed(2)} Yuan`);
+    $('.showExchange').text(`The exchange for Euro is ${(response.conversion_rates.EUR * dollar).toFixed(2)} Euro`);
+    $('.showExchange').text(`The exchange for Japanese Yen is ${(response.conversion_rates.JPY * dollar).toFixed(2)} Yen`);
+    $('.showExchange').text(`The exchange for Mexico Peso is ${(response.conversion_rates.MXN * dollar).toFixed(2)} Peso`);
+    $('.showExchange').text(`The exchange for Thai Baht is ${(response.conversion_rates.THB * dollar).toFixed(2)} Baht`);
+    $('.showExchange').text(`The exchange for China Yuan is ${(response.conversion_rates.CNY * dollar).toFixed(2)} Yuan`);
+    }
   } else {
     $('.showError').text(`There was a error: ${response}`);
   }
@@ -31,15 +40,15 @@ function clearFields() {
 }
 async function makeApiCall() {
   const response = await CurrencyService.currencyExchange();
-  getElements(response, USDollar, InputCurrency);
+  getElements(response);
 }
 
 $(document).ready(function() {
   $('#exchange').click(function() {
-    const USDollar = $('#USNumber').val();
-    const InputCurrency = $('#currency').val();
+    // let dollar = $('#USNumber').val();
+    // const inputCurrency = $('#currency').val();
     clearFields;
-    makeApiCall(response, USDollar, InputCurrency);
+    makeApiCall();
   });
 });
 
