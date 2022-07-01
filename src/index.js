6405a73c73f0b47cb60c09b55ae72a898b8af1dc
Peter Vang <vang_peter06@yahoo.com>
2022-07-01 10:48:59 -0700

    add Yen, Peso, Baht , Yuan output to JS

--- src/index.js
+++ src/index.js
@@ -9,20 +9,23 @@ import CurrencyService from './currencyService';
 function getElements(response) {
   const USDollar = $('#USNumber').val();
   if (response.conversion_rates) {
-    $('.showExchange').text(`The exchange for Euro is ${response.conversion_rates.EUR * USDollar}`)
+    $('.showExchange').text(`The exchange for Euro is ${(response.conversion_rates.EUR * USDollar).toFixed(2)} Euro`);
+    $('.showExchange').text(`The exchange for Japanese Yen is ${(response.conversion_rates.JPY * USDollar).toFixed(2)} Yen`);
+    $('.showExchange').text(`The exchange for Mexico Peso is ${(response.conversion_rates.MXN * USDollar).toFixed(2)} Peso`);
+    $('.showExchange').text(`The exchange for Thai Baht is ${(response.conversion_rates.THB * USDollar).toFixed(2)} Baht`);
+    $('.showExchange').text(`The exchange for China Yuan is ${(response.conversion_rates.CNY * USDollar).toFixed(2)} Yuan`);
   } else {
-    $('.showError').text(`There was a error: ${response}`)
+    $('.showError').text(`There was a error: ${response}`);
   }
 }
 
 async function makeApiCall() {
   const response = await CurrencyService.currencyExchange();
-  getElements(response)
+  getElements(response);
 }
 
 $(document).ready(function() {
   $('#exchange').click(function() {
-  
     makeApiCall();
   });
 });
