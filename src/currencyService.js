bf2817833252e0b587cd95934f5ab21374f0703a
Peter Vang <vang_peter06@yahoo.com>
2022-07-10 23:22:15 -0700

    WIP working on 200 ok

--- src/currencyService.js
+++ src/currencyService.js
@@ -3,7 +3,7 @@ export default class CurrencyService {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(response => {
         if (!response.ok){
-          throw Error("ERROR");
+          throw Error(response.statusText);
         }
         return response.json();
       })
